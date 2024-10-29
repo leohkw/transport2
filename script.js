1f4e39e200e08bcab335f294169fd3206deedc0c
leohkw <magicleoho@gmail.com>
2024-10-29 16:26:32 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const videoElements = [
+        { id: 'video-1', src: 'https://streaming1.dsatmacau.com/traffic/m2029.m3u8', thumbnailId: 'thumbnail-1', titleId: 'video-title-1' },
+        { id: 'video-2', src: 'https://streaming1.dsatmacau.com/traffic/m2092.m3u8', thumbnailId: 'thumbnail-2', titleId: 'video-title-2' }
+    ];
+
+    videoElements.forEach(video => {
+        const videoElement = document.getElementById(video.id);
+        const thumbnail = document.getElementById(video.thumbnailId);
+        const videoTitleElement = document.getElementById(video.titleId);
+        const canvas = document.createElement('canvas');
+        const context = canvas.getContext('2d');
+
+        if (Hls.isSupported()) {
+            const hls = new Hls();
+            hls.loadSource(video.src);
+            hls.attachMedia(videoElement);
+            hls.on(Hls.Events.MANIFEST_PARSED, () => {
+                videoElement.play().catch(error => {
+                    console.log('自动播放被阻止:', error);
+                });
+            });
+        } else if (videoElement.canPlayType('application/vnd.apple.mpegurl')) {
+            videoElement.src = video.src;
+            videoElement.addEventListener('loadedmetadata', () => {
+                videoElement.play().catch(error => {
+                    console.log('自动播放被阻止:', error);
+                });
+            });
+        }
+
+        videoElement.onloadeddata = () => {
+            canvas.width = videoElement.videoWidth;
+            canvas.height = videoElement.videoHeight;
+            context.drawImage(videoElement, 0, 0, canvas.width, canvas.height);
+            thumbnail.src = canvas.toDataURL('image/png');
+        };
+    });
+}); 
